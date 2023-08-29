19d8ece77aa760f483fd77f668f3db409dd34556
kazuki1023 <k.keio12569@gmail.com>
2023-08-29 10:58:07 +0900

    add squareコンポーネントにprops追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-function Square() {
-  return <button className="square">1</button>;
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
 export default function Board() {
   return (
